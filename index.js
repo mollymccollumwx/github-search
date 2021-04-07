9015af5d6bf23a08b625a9c960ee18a0035eb19c
Molly <mollymccollumwx@gmail.com>
2021-04-07 10:45:27 -0400

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// const btnRepos = document.getElementById("btnRepos");
-// btnRepos.addEventListener("click", getRepos);
-// function getRepos() {
-//     console.log("you clicked the button")
-// }
+const btnRepos = document.getElementById("btnRepos");
+btnRepos.addEventListener("click", getRepos);
+function getRepos() {
+    console.log("you clicked the button")
+}
 
-console.log("test")
