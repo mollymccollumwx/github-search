8c56bcf71dd152701fa1359a7959f01e48a3ee3a
Molly <mollymccollumwx@gmail.com>
2021-04-07 10:44:05 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello World')
+// const btnRepos = document.getElementById("btnRepos");
+// btnRepos.addEventListener("click", getRepos);
+// function getRepos() {
+//     console.log("you clicked the button")
+// }
+
+console.log("test")
