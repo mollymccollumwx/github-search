561f911344318fb08e90d9ef7f5592e3c895a1db
Molly <mollymccollumwx@gmail.com>
2021-04-09 17:57:44 -0400

    abstracted error message into it's own function to DRY up code

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const errorMessage = document.getElementById("error-message");
 
 // async function to preform GET request to Github API to collect ALL repos from inputted user
 async function getRepos() {
-
   const usernameInput = document.getElementById("input-username").value;
 
   clearSearch();
@@ -15,34 +14,24 @@ async function getRepos() {
   const url = "https://api.github.com/users/" + usernameInput + "/repos";
 
   const response = await fetch(url);
-  if(response.status >=200 && response.status <=299){
 
+  if (response.status >= 200 && response.status <= 299) {
     var result = await response.json();
-    // console.log(result)
-
+    // executes for each element in the array and appends it to the page
+    result.forEach((repo) => {
+      const link = document.createElement("a");
+      link.href = repo.html_url;
+      link.textContent = repo.name;
+      githubRepos.appendChild(link);
+      githubRepos.appendChild(document.createElement("br"));
+    });
   } else {
-    console.log(response.status);
-    const responseMessage = document.createElement("h5");
-    responseMessage.textContent =
-      "Username or repository not found. Please try again!";
-    errorMessage.appendChild(responseMessage);
+    printError();
   }
-  
-   // executes for each element in the array and appends it to the page
-   result.forEach((repo) => {
-    const link = document.createElement("a");
-    link.href = repo.html_url;
-    link.textContent = repo.name;
-    githubRepos.appendChild(link);
-    githubRepos.appendChild(document.createElement("br"));
-  });
-
- 
 }
 
 // async function to preform a GET request to the Github API to collect the commits from a particular repo based on user input
 async function getCommits() {
-
   const usernameInput = document.getElementById("input-username").value;
   const repoInput = document.getElementById("input-repo").value;
 
@@ -55,37 +44,29 @@ async function getCommits() {
     repoInput +
     "/commits";
 
-    const response = await fetch(url);
-    if(response.status >=200 && response.status <=299){
-  
-      var result = await response.json();
-      // console.log(result)
-  
-    } else {
-      console.log(response.status);
-      const responseMessage = document.createElement("h5");
-      responseMessage.textContent =
-        "Username or repository not found. Please try again!";
-      errorMessage.appendChild(responseMessage);
-    }
+  const response = await fetch(url);
 
-  result.forEach((repo) => {
-    // console.log(repo.commit.message);
-    const img = document.createElement("img");
-    img.src = repo.author.avatar_url;
-    const link = document.createElement("a");
-    link.href = repo.commit.html_url;
-    link.textContent = repo.commit.message;
-    const resultDiv = document.createElement("div");
-    resultDiv.appendChild(img);
-    resultDiv.appendChild(link).classList.add("ps-3");
-    githubInfo.append(resultDiv);
-    githubInfo.appendChild(document.createElement("br"));
-  });
+  if (response.status >= 200 && response.status <= 299) {
+    var result = await response.json();
+
+    result.forEach((repo) => {
+      const img = document.createElement("img");
+      img.src = repo.author.avatar_url;
+      const link = document.createElement("a");
+      link.href = repo.commit.html_url;
+      link.textContent = repo.commit.message;
+      const resultDiv = document.createElement("div");
+      resultDiv.appendChild(img);
+      resultDiv.appendChild(link).classList.add("ps-3");
+      githubInfo.append(resultDiv);
+      githubInfo.appendChild(document.createElement("br"));
+    });
+  } else {
+    printError();
+  }
 }
 // async function to preform a GET request to the Github API to collect the pull requests from a particular repo based on user input
 async function getPulls() {
-
   const usernameInput = document.getElementById("input-username").value;
   const repoInput = document.getElementById("input-repo").value;
 
@@ -98,43 +79,41 @@ async function getPulls() {
     repoInput +
     "/pulls";
 
-    const response = await fetch(url);
-    if(response.status >=200 && response.status <=299){
-  
-      var result = await response.json();
-      // console.log(result)
-  
-    } else {
-      console.log(response.status);
-      const responseMessage = document.createElement("h5");
-      responseMessage.textContent =
-        "Username or repository not found. Please try again!";
-      errorMessage.appendChild(responseMessage);
+  const response = await fetch(url);
+  if (response.status >= 200 && response.status <= 299) {
+    var result = await response.json();
+
+    if (result.length === 0) {
+      const nullMessage = document.createElement("h5");
+      nullMessage.textContent =
+        "No pull requests found for the repository: " + repoInput;
+      errorMessage.appendChild(nullMessage);
     }
 
-  // console.log(result);
-
-  if (result.length === 0) {
-    // console.log("no pull requests found");
-    const nullMessage = document.createElement("h5");
-    nullMessage.textContent =
-      "No pull requests found for the repository: " + repoInput;
-    errorMessage.appendChild(nullMessage);
+    result.forEach((pull) => {
+      const link = document.createElement("a");
+      link.href = pull.html_url;
+      link.textContent = pull.title;
+      githubInfo.appendChild(link);
+      githubInfo.appendChild(document.createElement("br"));
+    });
+  } else {
+    printError();
   }
-
-  result.forEach((pull) => {
-    const link = document.createElement("a");
-    link.href = pull.html_url;
-    link.textContent = pull.title;
-    githubInfo.appendChild(link);
-    githubInfo.appendChild(document.createElement("br"));
-  });
 }
 
 // function to clear the prior results or messages if there are any
 function clearSearch() {
-  while (githubRepos.firstChild) githubRepos.removeChild(githubRepos.firstChild);
+  while (githubRepos.firstChild)
+    githubRepos.removeChild(githubRepos.firstChild);
   while (githubInfo.firstChild) githubInfo.removeChild(githubInfo.firstChild);
-  while (errorMessage.firstChild) errorMessage.removeChild(errorMessage.firstChild);
+  while (errorMessage.firstChild)
+    errorMessage.removeChild(errorMessage.firstChild);
 }
 
+function printError() {
+  const responseMessage = document.createElement("h5");
+  responseMessage.textContent =
+    "Username or repository not found. Please try again!";
+  errorMessage.appendChild(responseMessage);
+}
